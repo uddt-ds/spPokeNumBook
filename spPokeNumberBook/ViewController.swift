d64b3d1f5ed430c0390615848f462bf929539ea6
uddt <uddt_ds@kakao.com>
2025-04-17 07:35:48 +0900

    feat(label, button): 친구 목록 Label과 추가 버튼의 UI를 생성하였습니다. #1
    - 변경(Setting)
    코드베이스 UI 작업을 위해
    SceneDelegate를 수정하였습니다.
    
    - 추가(UI)
    nameLabel
    addButton

--- spPokeNumberBook/ViewController.swift
+++ spPokeNumberBook/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "친구 목록"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 30)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("추가", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.backgroundColor = .white
+//        button.addTarget(self, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+        return button
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    func configureUI() {
+        view.backgroundColor = .white
+        [label, addButton].forEach { view.addSubview($0) }
 
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(70)
+            $0.centerX.equalToSuperview()
+        }
+
+        addButton.snp.makeConstraints {
+            $0.centerY.equalTo(label.snp.centerY)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+    }
 }
-
